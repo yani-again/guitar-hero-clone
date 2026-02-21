ee00fc132708b17da1b7496ed87b722e1615bb20
yani-again <historicalsection83@proton.me>
2026-02-21 20:43:22 +0000

    finished second stage boot with QSPI

---
+++ README.md
@@ -0,0 +1,54 @@
+# Guitar Hero Clone Game
+This project is a student-led project that combines our skills & interests to produce a playable experience modelled after the hit game Guitar Hero.
+
+It uses:
+- Python & PyGame for the playable game experience
+- Raspberry Pi RP2040 for the game controller
+- bare-metal C to program the game controller
+
+Below are further details on both halves of the project.
+
+## Game (Python & PyGame)
+*Programmers: @ar-eej, @tej-333, @sophiehardwick0*
+```
+TODO: complete this section when designs are finalised & coding begins.
+```
+
+## Game Controller (Bare-Metal RP2040 In C)
+*Programmers: @yani-again, @*
+### What Is This?
+The code within the `microcontroller` folder is the code for an RP2040 Guitar Hero-inspired 'guitar'. It uses bare-metal C to implement minimal startup code and a USB stack for communication with the other half of this project - a Guitar Hero-inspired game written in Python's PyGame library.
+
+It does not make use of Raspberry Pi's SDK, and requires no external libraries, just certain standard C libraries *(such as `stdint.h`).*
+
+### Project Motivation
+This project started when I wanted to learn about and implement the USB stack from scratch, without using pre-built ones like [TinyUSB](https://docs.tinyusb.org/en/latest/) and without the Raspberry Pi SDK (instead opting in to use bare-metal C).
+
+**But, why not at least use the SDK?**
+
+Because ~~I have a lot of time on my hands~~ I believe that building something from the ground-up is the best way to learn how it works *(and I wanted an interesting project)*.
+
+However, what I didn't know at the time is skipping the SDK meant skipping all of the pre-written startup code. I'm still stubborn enough to stick to my promise of skipping the SDK, so that meant I had to learn concepts like:
+- the RP2040 boot sequence
+- minimal Assembly for the ARM Cortex-M0+
+- SPI/QSPI communication
+- CRC32 checksums
+- vector tables
+- reset handlers
+- linker scripts
+Since most *(reasonable)* people just opt for the SDK - or at the very least opt for using pre-written startup code - I had to mostly rely on documentation and reading pre-written code to understand what's happening and, perhaps more importantly, *why* it's happening, so I've linked what I mostly relied on at the bottom of this file.
+
+### Sources
+These are the sources I references a lot while programming this:
+#### Written Sources
+- [RP2040 Datasheet](https://pip-assets.raspberrypi.com/categories/814-rp2040/documents/RP-008371-DS-1-rp2040-datasheet.pdf)
+- [The Definitive Guide To ARM Cortex-M0 And Cortex-M0+ Processors by Joseph Yiu](https://www.sciencedirect.com/book/monograph/9780128032770/the-definitive-guide-to-arm-cortex-m0-and-cortex-m0-and-processors)
+- [SPI NOR Flash Standard](https://www.winbond.com/hq/product/code-storage-flash-memory/serial-nor-flash/?__locale=en&partNo=W25Q80DV)
+- [GCC documentation](https://gcc.gnu.org/onlinedocs)
+- [Universal Serial Bus Specification](https://wcours.gel.ulaval.ca/GIF1001/old/h20/docs/USB_20.pdf)
+- [USB - Device Class Definition for Human Interface Devices (HID)](https://www.usb.org/sites/default/files/hid1_11.pdf)
+#### Other Repositories
+- [vxj9800/bareMetalRP2040](https://github.com/vxj9800/bareMetalRP2040)
+- [carlosftm/RPi-Pico-Baremetal](https://github.com/carlosftm/RPi-Pico-Baremetal)
+
+**A huge thank you to all of the dedicated people for these resources! It wouldn't be possible without them.**
--- microcontroller/boot2/boot2_quad_io.c
+++ microcontroller/boot2/boot2_quad_io.c
@@ -1,63 +1,97 @@
-/* written sources: (move these to README.md later)
- * RP2040 Datasheet
- * https://pip-assets.raspberrypi.com/categories/814-rp2040/documents/RP-008371-DS-1-rp2040-datasheet.pdf
- *
- * The Definitive Guide To ARM Cortex-M0 And Cortex-M0+ Processors by Joseph Yiu
- * https://www.sciencedirect.com/book/monograph/9780128032770/the-definitive-guide-to-arm-cortex-m0-and-cortex-m0-and-processors
- *
- * SPI NOR Flash Standard (for register offsets)
- * https://www.winbond.com/hq/product/code-storage-flash-memory/serial-nor-flash/?__locale=en&partNo=W25Q80DV
- *
- * MSVC /SECTION documentation
- * https://github.com/MicrosoftDocs/cpp-docs/blob/main/docs/build/reference/section-specify-section-attributes.md
- */
-
 #include <stdint.h>
+
 
 // XIP
 #define XIP_BASE            0x10000000
 
 // SSI
 #define XIP_SSI_BASE        0x18000000
-#define XIP_SSI_CTRLR0      ( *(volatile uint32_t *) ) (SSI_BASE + 0x00)
-#define XIP_SSI_SSIENR      ( *(volatile uint32_t *) ) (SSI_BASE + 0x08)
-#define XIP_SSI_BAUDR       ( *(volatile uint32_t *) ) (SSI_BASE + 0x14)
-#define XIP_SSI_SR          ( *(volatile uint32_t *) ) (SSI_BASE + 0x28)
-#define XIP_SSI_DR0         ( *(volatile uint32_t *) ) (SSI_BASE + 0x60)
-#define XIP_SSI_SPI_CTRLR0  ( *(volatile uint32_t *) ) (SSI_BASE + 0xf4)
+#define XIP_SSI_CTRLR0      ( *(volatile uint32_t *) (XIP_SSI_BASE + 0x00) )
+#define XIP_SSI_SSIENR      ( *(volatile uint32_t *) (XIP_SSI_BASE + 0x08) )
+#define XIP_SSI_BAUDR       ( *(volatile uint32_t *) (XIP_SSI_BASE + 0x14) )
+#define XIP_SSI_SR          ( *(volatile uint32_t *) (XIP_SSI_BASE + 0x28) )
+#define XIP_SSI_DR0         ( *(volatile uint32_t *) (XIP_SSI_BASE + 0x60) )
+#define XIP_SSI_SPI_CTRLR0  ( *(volatile uint32_t *) (XIP_SSI_BASE + 0xf4) )
+
+// QSPI values
+#define QSPI_ENABLE         2       // switch SPI to QSPI I/O
+#define QSPI_CLOCKS         31      // 32 clocks per frame
+#define QSPI_TRANS_MODE     3       // EEPROM read mode
+#define QSPI_INST_LEN       2       // 8-bit instructions
+#define QSPI_INST_PREFIX    0x6B
+#define QSPI_WAIT           8       // clocks to wait between TX & RX
+#define QSPI_ADDR_LEN       6       // 24-bit addrs
 
 // SPI flash standard values
-#define SPI_SR1_R   0x05
-#define SPI_SR2_R   0x35
-#define SPI_SR_W    0x01
+#define SPI_SR1_R           0x05
+#define SPI_SR2_R           0x35
+#define SPI_SR_W            0x01
 
-/* Notes:
- * disable SSI before configuring it
- * clk_peri runs at 125MHz at startup (SSI runs from it)
- * DFS_32 = how many bits of data are transferred per frame
- */
+// M0PLUS
+#define M0PLUS_BASE         0xE0000000
+#define M0PLUS_VTOR         ( *(volatile uint32_t *) (M0PLUS_BASE + 0xED08) )
+#define VECTOR_TABLE_BASE   XIP_BASE + 0x100
+#define VECTOR_TABLE_SP     VECTOR_TABLE_BASE
+#define VECTOR_TABLE_RST    VECTOR_TABLE_BASE + 0x04    // reset handler
 
-#ifndef _MSC_VER
-    __attribute__((noreturn, section(".boot2")))
+
+// compiler attributes
+#if defined(__GNUC__) || defined(__clang__)
+#define BOOT2_ATTR __attribute__((noreturn, section(".boot2")))
 #else
-    #error "Please compile with a compiler that \
-        supports \"__attribute__\" (e.g. GCC/Clang)"
+#error "Compiler must support GNU-style \"__attribute__\""
 #endif
+
+
+BOOT2_ATTR
 void boot_stage_2(void)
 {
+    // initial SSI config
     XIP_SSI_SSIENR = 0;
     XIP_SSI_BAUDR = 4;
-    XIP_SSI_CTRLR0 |= (7 << 16);    // 8 clocks per data frame
+    XIP_SSI_CTRLR0 |= (7 << 16);    // 8 clocks per frame for 8-bit
+                                    // SR registers
     XIP_SSI_SSIENR = 1;
 
     // check if QE bit is on in SR2, enable if not
     XIP_SSI_DR0 = SPI_SR1_R;
     XIP_SSI_DR0 = SPI_SR2_R;
+    while ((~XIP_SSI_SR & (1 << 2)) || (XIP_SSI_SR & 1))
+        ;   // wait for transmission to finish
+    uint8_t status_reg1 = XIP_SSI_DR0;
+    uint8_t status_reg2 = XIP_SSI_DR0;
+    if (~status_reg2 & (1 << 1))
+    {
+        XIP_SSI_DR0 = 0x06; // enable write
+        XIP_SSI_DR0 = 0x01; // write command
+        XIP_SSI_DR0 = status_reg1;
+        XIP_SSI_DR0 = status_reg2 | (1 << 1);
+        XIP_SSI_DR0 = 0x04; // disable write
+    }
 
+    // configure QSPI
     XIP_SSI_SSIENR = 0;
-    XIP_SSI_CTRLR0 |= (3 << 21)     // QSPI mode (4-channels)
-                      | (31 << 16)  // 32 clocks per data frame
-                      | (3 << 8);   // EEPROM read mode
+    XIP_SSI_CTRLR0 |= (QSPI_ENABLE << 21)
+                    | (QSPI_CLOCKS << 16)
+                    | (QSPI_TRANS_MODE << 8);
+    XIP_SSI_SPI_CTRLR0 |= (QSPI_INST_PREFIX << 24)
+                        | (QSPI_WAIT << 11)
+                        | (QSPI_INST_LEN << 8)
+                        | (QSPI_ADDR_LEN << 2);
     XIP_SSI_SSIENR = 1;
 
+    // load SP & call reset handler using vector table
+    M0PLUS_VTOR = VECTOR_TABLE_BASE;
+    
+    asm("MSR MSP, %0"
+            :
+            : "r" ( *(uint32_t *) (M0PLUS_VTOR + 0x00) )
+        );
+    asm("BX %0"
+            :
+            : "r" ( *(uint32_t *) (M0PLUS_VTOR + 0x04) )
+        );
+
+    __builtin_unreachable();
 }
+
